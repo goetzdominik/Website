9e3575240db3bdaceea2744d30cc325cf95281db
Dominik Götz <goetzdominik@gmx.net>
2024-01-24 10:33:00 +0100

    fixefd gallery loading problem

--- app/Gallery/page.js
+++ app/Gallery/page.js
@@ -20,7 +20,7 @@ const Gallery = () => {
             <div className="overlay_text">Had a great time in the BCB III: OC lab</div>
           </div>
           <div className="gallery_card">
-            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/board_small.heic" alt="2" fill={true}/>
+            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/board_small.jpg" alt="2" fill={true}/>
             <div className="overlay_text">Board from my lesson on molecular orbitals</div>
           </div>
         </div>
@@ -47,7 +47,7 @@ const Gallery = () => {
         <div className="grid gap-4">
           {/* Column 1 */}
           <div className="gallery_card">
-            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/bike_small.HEIC" alt="1" fill={true}/>
+            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/bike_small.jpg" alt="1" fill={true}/>
             <div className="overlay_text">In summer you will find me on the road with my bike </div>
           </div>
           {/* Column 2 */}
@@ -56,7 +56,7 @@ const Gallery = () => {
             <div className="overlay_text">Award for best poster and presentation in Blockkurs: Research Cycle in Genomics</div>
           </div>
           <div className="gallery_card">
-            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/sunset_small.HEIC" alt="2" fill={true}/>
+            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/sunset_small.jpg" alt="2" fill={true}/>
             <div className="overlay_text">One of the countless stunning sunsets at ETH Hönggerberg</div>
           </div>
         </div>
@@ -74,35 +74,12 @@ const Gallery = () => {
             <div className="overlay_text">Most sucessful event I coordinated, VCS plays paintball against VMP</div>
           </div>
           <div className="gallery_card">
-            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/bib_big.HEIC" alt="2" fill={true}/>
+            <Image radius="lg"  className="image_content" placeholder='blur' src="/img/Gallery/bib_big.jpg" alt="2" fill={true}/>
             <div className="overlay_text">The library at ETH Hönggerberg, my favorite study spot</div>
           </div>
         </div>
       </div>
 
-      <style jsx>{`
-        .gallery_card {
-          position: relative;
-          overflow: hidden;
-        }
-
-        .gallery_card:hover .image_content {
-          filter: blur(8px);
-        }
-
-        .overlay_text {
-          position: absolute;
-          bottom: 10px;
-          left: 10px;
-          color: white;
-          font-size: 20px;
-          display: none;
-        }
-
-        .gallery_card:hover .overlay_text {
-          display: block;
-        }
-      `}</style>
     </div>
   );
 };
