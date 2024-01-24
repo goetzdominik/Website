26863775a2589d501881e5004268e573ba0621ae
Dominik Götz <goetzdominik@gmx.net>
2024-01-24 10:22:53 +0100

    removed focus

--- components/Navbar_new.js
+++ components/Navbar_new.js
@@ -34,7 +34,7 @@ const Navbar = () => {
                     <button
                         type="button"
                         onClick={handleMenuToggle}
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-sticky"
                         aria-expanded={isMenuOpen}
                     >
