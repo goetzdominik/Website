7b0d5aea2806434ce78a78ef762863f7d627b3ac
Dominik Götz <goetzdominik@gmx.net>
2024-02-02 15:26:54 +0100

    New card arrangement

--- components/Cards.js
+++ components/Cards.js
@@ -4,8 +4,58 @@ import {Image} from "@nextui-org/react";
 const Cards = () => {
     return ( 
         <div>
-            <h3 className=" page_subtitle  ">Currently</h3>
+            <h3 className=" page_subtitle  ">Teaching</h3>
             <div className="mt-8 grid grid-cols-1    sm:grid-cols-2 lg:grid-cols-4 gap-8  text-primary dark:text-gray-100" >
+                        
+            
+              
+            <Link className = "card  "   href = "/Teaching/HS22">
+                    <Image isZoomed src="/img/Main/info.jpg" alt="InforamticsI" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
+                        <div className = "m-4">
+                            <span className =" font-bold"> Informatics I  </span>
+                            <span className = "block text-secondary dark:text-gray-400 "> Slides and Ankis for the lecture.</span>
+                        </div>
+                        <div className = "badge_left">
+                            <span>HS22</span>
+                            
+                        </div>
+                        <div className = "badge_right">
+                            <span>AI generated</span>
+                            
+                        </div>
+                        
+                    </Link>
+            <Link  className = "card  "   href = "/Teaching/FS23">
+                    <Image isZoomed src="/img/Main/ACACII.jpg" alt="ACACII" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
+                        <div className = "m-4">
+                            <span className =" font-bold"> ACAC II </span>
+                            <span className = "block text-secondary dark:text-gray-400 "> Slides and useful Documents.   </span>
+                        </div>
+                        <div className = "badge_left">
+                            <span>FS23</span>
+                            
+                        </div>
+                        <div className = "badge_right">
+                            <span>AI generated</span>
+                            
+                        </div>
+                    </Link>
+            <Link className = "card   "  href = "/Teaching/HS23">
+                        <Image  isZoomed src="/img/Main/ACOCI.jpg"  alt="ACOCI" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
+                        
+                        <div className = "m-4">
+                            <span className =" font-bold"> ACOC I </span>
+                            <span className = "block text-secondary dark:text-gray-400"> All slides and solutions.</span>
+                        </div>
+                        <div className = "badge_left">
+                            <span>HS23</span>
+                            
+                        </div>
+                        <div className = "badge_right">
+                            <span>AI generated</span>
+                            
+                        </div>
+                    </Link>
 
                     <Link className = "card   "  href = "/Teaching/FS24">
                         <Image isZoomed src="/img/Main/ACOCII.jpg" fallbackSrc = "/img/Placeholder/filler_2.png"
@@ -24,31 +74,7 @@ const Cards = () => {
                             
                         </div>
                     </Link>
-                    <Link className = "card  "   href = "/Presentation">
-                    <Image isZoomed src="/img/Main/RBTL_big.jpg" fallbackSrc = "/img/Placeholder/filler_2.png" alt="Presentation"fill={true}/>
-                        <div className = "m-4">
-                            <span className =" font-bold"> Presentations </span>
-                            <span className = "block text-secondary dark:text-gray-400 "> All presentation accompanying posters.  </span>
-                        </div>
-                    </Link>
-                    <Link className = "card  "  href ="/Contact">
-                    <Image isZoomed src="/img/Main/contact.jpg" fallbackSrc = "/img/Placeholder/filler_2.png"alt="Contact"fill={true}/>
-                        <div className = "m-4">
-                            <span className =" font-bold"> Contact Me</span>
-                            <span className = "block text-secondary dark:text-gray-400 ">  Get in Touch with me!</span>
-                        </div>
-                        <div className = "badge_right">
-                            <span>AI generated</span>
-                            
-                        </div>
-                    </Link>
-                    <Link className = "card  "  href ="/About">
-                    <Image isZoomed src="/img/Main/dominik.jpg" fallbackSrc = "/img/Placeholder/filler_2.png" alt="About" fill={true}/>
-                        <div className = "m-4">
-                            <span className =" font-bold"> About Me</span>
-                            <span className = "block text-secondary dark:text-gray-400 ">  More information about this website and the content.</span>
-                        </div>
-                    </Link>
+                   
                 </div>
         </div>
 
