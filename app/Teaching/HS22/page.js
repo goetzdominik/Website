eb04587a121cc4cdbe4ffe9b70e569fa3eabc354
Dominik Götz <goetzdominik@gmx.net>
2024-02-02 12:10:58 +0100

    icons fixed all

--- app/Teaching/HS22/page.js
+++ app/Teaching/HS22/page.js
@@ -27,7 +27,7 @@ const HS22 = () => {
             </div>
         </div>
         <div className=" text_card_grid">
-            <div className =" md:w-1/3 text_card grid-cols-[auto_100px] ">
+            <div className =" md:w-1/3 text_card_icon grid-cols-[auto_100px] ">
                 
                 <div>
                     <div>
@@ -42,7 +42,7 @@ const HS22 = () => {
                     </div>
                 </div>
                 <div>
-                    <Link href="/download/HS22/Anki.zip" className = "ml-8 pt-2 p-4  text-primary hover:text-gray-500 dark:hover:text-blue-500  rounded-lg " role="button" target="_blank">
+                    <Link href="/download/HS22/Anki.zip" className = "icon_text_card " role="button" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                             <path fillRule="evenodd" d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V10.5Z" clipRule="evenodd" />
                         </svg>
@@ -51,8 +51,8 @@ const HS22 = () => {
             </div>
             
 
-            <div className =" md:w-2/3 text_card">
-            <div>
+            <div className =" md:w-2/3 text_card_icon">
+                <div>
                     <h2>
                     Padlett
                     </h2>
@@ -64,7 +64,7 @@ const HS22 = () => {
                     </p>
                 </div>
                 <div>
-                    <Link href="https://padlet.com/dogoetz/informatik-1-padlet-o6g8187ziqq88diz" className = " pt-2 p-4  text-primary hover:text-gray-500 dark:hover:text-blue-500  rounded-lg " role="button" target="_blank">
+                    <Link href="https://padlet.com/dogoetz/informatik-1-padlet-o6g8187ziqq88diz" className = "icon_text_card" role="button" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                             <path fillRule="evenodd" d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97-1.94.284-3.916.455-5.922.505a.39.39 0 0 0-.266.112L8.78 21.53A.75.75 0 0 1 7.5 21v-3.955a48.842 48.842 0 0 1-2.652-.316c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97Z" clipRule="evenodd" />
                       </svg>
--- app/Teaching/HS23/page.js
+++ app/Teaching/HS23/page.js
@@ -25,7 +25,7 @@ const HS23 = () => {
                 </div>
             </div>
             <div className=" text_card_grid">
-                <div className =" md:w-1/3 text_card ">
+                <div className =" md:w-1/3 text_card_icon ">
                     <div>
                         <h2>
                             Ankis
@@ -39,7 +39,7 @@ const HS23 = () => {
                         </p>
                         </div>
                     <div>
-                    <Link href="/download/HS23/ACOC_I_Anki .apkg" className = " pt-2 p-4  text-primary hover:text-gray-500 dark:hover:text-blue-500  rounded-lg " role="button" target="_blank">
+                    <Link href="/download/HS23/ACOC_I_Anki .apkg" className = "icon_text_card" role="button" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                 <path fill-rule="evenodd" d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V10.5Z" clip-rule="evenodd" />
                             </svg>
