e2f63d8d58d85ca03b951e316ffc86a83fc57d3f
Dominik Götz <goetzdominik@gmx.net>
2024-02-03 11:28:15 +0100

    now page skelette

--- app/Now/page.js
+++ app/Now/page.js
@@ -1,20 +1,28 @@
 const Now = () => {
     return ( 
-        <div className=" flex flex-wrap justify-center">   
+        <div className="text-center">   
             <h1 className = "page_title">Now</h1>
-        <div className =" flex flex-wrap"> 
-            <div>
-          
+            <p>Updated February 3, from some where</p>
+            <p>This part is still underconstruction</p>
+            {/* <div className="felx flex-row ">
+                <div className="flex flex-row justify-between items-baseline">
+                    <h1 className="page_subtitle ">Prüfungspahse</h1>
+                    <p>03.02.2024</p>
+                </div>
+                <div>
+                    some Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, quo quas doloremque quis quam incidunt vero ex ab. Accusantium magnam laboriosam fuga aperiam, veritatis dolor numquam ab? Id, aut cum?
+                </div>
             </div>
-       
-        <div>
-            <p className = "text-4xl  pt-4 pl-4  ">
-                Ooops..... Seems like this part is still under construction.
-            </p>
+            <div className="felx flex-row">
+                <div className="flex flex-col-2">
+                    <h1 className="page_subtitle">Prüfungspahse</h1>
+                    <p>03.02.2024</p>
+                </div>
+                <div>
+                    some Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, quo quas doloremque quis quam incidunt vero ex ab. Accusantium magnam laboriosam fuga aperiam, veritatis dolor numquam ab? Id, aut cum?
+                </div>
+            </div> */}
         </div>
-        </div>
-
-    </div>
      );
 }
  
