db45f4a2b1d38c00cb3e69074fc05fd019974ac9
Dominik Götz <goetzdominik@gmx.net>
2024-02-03 16:20:55 +0100

    ordered cards

--- components/Cards.js
+++ components/Cards.js
@@ -8,56 +8,7 @@ const Cards = () => {
             <div className="mt-8 grid grid-cols-1    sm:grid-cols-2 lg:grid-cols-4 gap-8  text-primary dark:text-gray-100" >
                         
             
-              
-            <Link className = "card  "   href = "/Teaching/HS22">
-                    <Image isZoomed src="/img/Main/info.jpg" alt="InforamticsI" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
-                        <div className = "m-4">
-                            <span className =" font-bold"> Informatics I  </span>
-                            <span className = "block text-secondary dark:text-gray-400 "> Slides and Ankis for the lecture.</span>
-                        </div>
-                        <div className = "badge_left">
-                            <span>HS22</span>
-                            
-                        </div>
-                        <div className = "badge_right">
-                            <span>AI generated</span>
-                            
-                        </div>
-                        
-                    </Link>
-            <Link  className = "card  "   href = "/Teaching/FS23">
-                    <Image isZoomed src="/img/Main/ACACII.jpg" alt="ACACII" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
-                        <div className = "m-4">
-                            <span className =" font-bold"> ACAC II </span>
-                            <span className = "block text-secondary dark:text-gray-400 "> Slides and useful Documents.   </span>
-                        </div>
-                        <div className = "badge_left">
-                            <span>FS23</span>
-                            
-                        </div>
-                        <div className = "badge_right">
-                            <span>AI generated</span>
-                            
-                        </div>
-                    </Link>
-            <Link className = "card   "  href = "/Teaching/HS23">
-                        <Image  isZoomed src="/img/Main/ACOCI.jpg"  alt="ACOCI" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
-                        
-                        <div className = "m-4">
-                            <span className =" font-bold"> ACOC I </span>
-                            <span className = "block text-secondary dark:text-gray-400"> All slides and solutions.</span>
-                        </div>
-                        <div className = "badge_left">
-                            <span>HS23</span>
-                            
-                        </div>
-                        <div className = "badge_right">
-                            <span>AI generated</span>
-                            
-                        </div>
-                    </Link>
-
-                    <Link className = "card   "  href = "/Teaching/FS24">
+            <Link className = "card   "  href = "/Teaching/FS24">
                         <Image isZoomed src="/img/Main/ACOCII.jpg" fallbackSrc = "/img/Placeholder/filler_2.png"
                         alt="NextUI hero Image with delay"  placeholder='blur' fill={true}/>
                         
@@ -74,6 +25,56 @@ const Cards = () => {
                             
                         </div>
                     </Link>
+                     <Link className = "card   "  href = "/Teaching/HS23">
+                        <Image  isZoomed src="/img/Main/ACOCI.jpg"  alt="ACOCI" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
+                        
+                        <div className = "m-4">
+                            <span className =" font-bold"> ACOC I </span>
+                            <span className = "block text-secondary dark:text-gray-400"> All slides and solutions.</span>
+                        </div>
+                        <div className = "badge_left">
+                            <span>HS23</span>
+                            
+                        </div>
+                        <div className = "badge_right">
+                            <span>AI generated</span>
+                            
+                        </div>
+                    </Link>
+          
+            <Link  className = "card  "   href = "/Teaching/FS23">
+                    <Image isZoomed src="/img/Main/ACACII.jpg" alt="ACACII" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
+                        <div className = "m-4">
+                            <span className =" font-bold"> ACAC II </span>
+                            <span className = "block text-secondary dark:text-gray-400 "> Slides and useful Documents.   </span>
+                        </div>
+                        <div className = "badge_left">
+                            <span>FS23</span>
+                            
+                        </div>
+                        <div className = "badge_right">
+                            <span>AI generated</span>
+                            
+                        </div>
+                    </Link>
+           
+                    <Link className = "card  "   href = "/Teaching/HS22">
+                    <Image isZoomed src="/img/Main/info.jpg" alt="InforamticsI" fallbackSrc = "/img/Placeholder/filler_2.png"  fill={true}/>
+                        <div className = "m-4">
+                            <span className =" font-bold"> Informatics I  </span>
+                            <span className = "block text-secondary dark:text-gray-400 "> Slides and Ankis for the lecture.</span>
+                        </div>
+                        <div className = "badge_left">
+                            <span>HS22</span>
+                            
+                        </div>
+                        <div className = "badge_right">
+                            <span>AI generated</span>
+                            
+                        </div>
+                        
+                    </Link>
+                   
                    
                 </div>
         </div>
--- components/Navbar_new.js
+++ components/Navbar_new.js
@@ -2,6 +2,7 @@ import { Lora } from 'next/font/google'
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import {Burger} from './burger';
 
 import {
     Dropdown,
@@ -32,36 +33,32 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="backdrop-blur-lg  fixed flex w-full z-30 top-0 start-0 items-center justify-center">
+        <nav className="backdrop-blur-lg    text-primary fixed flex w-full z-30 top-0 start-0 items-center justify-center">
             <div className={lora.className}>
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-center pt-4 p-1 lg:p-4">
+            <div className="   flex flex-wrap items-center justify-center pt-4 p-1 lg:p-4">
                 <Link href="/" onClick={closeMenu} className="flex items-center mt-1 hover:text-gray-500 space-x-3 rtl:space-x-reverse   p-4">
                     <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Dominik Götz</span>
                 </Link>
                 <div className="flex lg:order-2 i space-x-3 lg:space-x-0 rtl:space-x-reverse">
-                    <button
-                        type="button"
-                        onClick={handleMenuToggle}
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg lg:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-sticky"
-                        aria-expanded={isMenuOpen}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 mt-1 text-primary hover:text-gray-500 h-6">
-                            <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
-                        </svg>
+                    
+                    <Button isIconOnly color="warning" variant="faded" aria-label="Take a photo"
+                    onClick={handleMenuToggle}
+                    className='lg:hidden '
+                    aria-controls="navbar-sticky"
+                    radius="full"
+                    aria-expanded={isMenuOpen}>
 
-                    </button>
+                        <Burger/>
+
+                    </Button>
                 </div>
                 <div
-                        className={`${
-                            isMenuOpen ? 'flex flex-col items-center' : 'hidden'
-                        } w-full text-2xl p-4 pb-2 lg:flex lg:w-auto lg:order-1`}
-                        id="navbar-sticky"
+                       className={`${isMenuOpen ? 'flex flex-col items-center h-screen' : 'hidden'} w-full text-2xl p-4 pb-2 lg:flex lg:w-auto lg:order-1 lg:h-auto`}
+
                     >
-                    <ul className="flex flex-col lg:flex-row items-center lg:p-0 font-medium lg:space-x-8 rtl:space-x-reverse dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col   lg:flex-row items-center lg:p-0 font-medium lg:space-x-8 rtl:space-x-reverse dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
                         <li className='pt-2'>
-                            <Link href="/" onClick={closeMenu} className="block py-1 px-3 text-primary   hover:text-gray-500 rounded  lg:hover:bg-transparent lg:p-0 ">
+                            <Link href="/" onClick={closeMenu} className="block py-1 px-3   hover:text-gray-500 rounded  lg:hover:bg-transparent lg:p-0 ">
                                 
                                 Home</Link>
                         </li>
@@ -80,19 +77,19 @@ const Navbar = () => {
                                     </Button>
                                 </DropdownTrigger>
                                 <DropdownMenu  className={lora.className} aria-label="Static Actions">
-                                    <DropdownItem className=" border-b-2 " key="new">
+                                    <DropdownItem className=" border-b-2 " key="FS24">
                                         <Link href="/Teaching/FS24" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">ACOC II</Link>
                                     </DropdownItem>
-                                    <DropdownItem key="copy">
+                                    <DropdownItem key="HS23">
                                         <Link href="/Teaching/HS23" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">ACOC I</Link>
                                     </DropdownItem>
-                                    <DropdownItem key="edit">
+                                    <DropdownItem key="FS23">
                                         <Link href="/Teaching/FS23" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">ACAC II</Link>
                                     </DropdownItem>
-                                    <DropdownItem className=" border-b-2 " key="delete" >
+                                    <DropdownItem className=" border-b-2 " key="HS22" >
                                         <Link href="/Teaching/HS22" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">Informatics I </Link>
                                     </DropdownItem>
-                                    <DropdownItem key="delete" >
+                                    <DropdownItem key="library" >
                                         <Link href="/Library" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">Library </Link>
                                     </DropdownItem>
                                 </DropdownMenu>
@@ -117,16 +114,17 @@ const Navbar = () => {
                                     </Button>
                                 </DropdownTrigger>
                                 <DropdownMenu className={lora.className} aria-label="Static Actions">
-                                    <DropdownItem className=" " key="">
-                                        <Link href="/About" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">About</Link>
-                                    </DropdownItem>
-                                    <DropdownItem key="edit">
+                                   
+                                    <DropdownItem key="now">
                                         <Link href="/Now" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">Now</Link>
                                     </DropdownItem>
-                                    <DropdownItem key="edit">
+                                    <DropdownItem className=" " key="about">
+                                        <Link href="/About" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">About</Link>
+                                    </DropdownItem>
+                                    <DropdownItem key="gallary">
                                         <Link href="/Gallery" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">Gallery</Link>
                                     </DropdownItem>
-                                    <DropdownItem key="copy">
+                                    <DropdownItem key="contact">
                                         <Link href="/Contact" onClick={closeMenu} className="block hover:bg-gray-300 p-2 rounded-lg  ">Contact</Link>
                                     </DropdownItem>
                                 </DropdownMenu>
