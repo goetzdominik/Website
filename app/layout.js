7e74cf89ab785b2ba1b8c94435d79cff9c6e6438
Dominik Götz <goetzdominik@gmx.net>
2024-02-03 16:20:26 +0100

    data for scraper

--- app/layout.js
+++ app/layout.js
@@ -28,14 +28,19 @@ export default function RootLayout({ children }) {
       <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png"/>
       <title>Dominik Götz - ETH</title>
+      <meta name="description" content="Homepage for my teaching at ETH and a way to share information about what I am doing." />
+      <meta property="og:title" content="Dominik Götz - ETH" />
+      <meta property="og:description" content="Homepage for my teaching at ETH and a way to share information about what I am doing." />
+      <meta property="og:image" content="/img/favicon-32x32.png" />
+      <meta property="og:url" content="https://www.dgoetz.net" />
       </head>
       
       <body className={merriweather.className}>
       <NextUIProvider>
       <div className = "min-h-screen relative text-primary ">
           <div className=' ' >
-          <Navbar/>
-          {/* <Navbar_new/> */}
+          {/* <Navbar/> */}
+          <Navbar_new/>
 
           <div className="flexflex-col items-center justify-between pb-16 pl-6 pr-6 pt-24  sm:pl-12 md:pl-24 lg:pl-36 sm:pr-12 md:pr-24 lg:pr-36 sm:pb-6 md:pb-12 sm:pt-12 md:pt-24">
           {children}
