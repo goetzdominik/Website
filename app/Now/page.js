cc41257281a56448f556880a11a3c013faa254b7
Dominik Götz <goetzdominik@gmx.net>
2024-02-03 16:46:45 +0100

    Now page

--- app/Now/page.js
+++ app/Now/page.js
@@ -1,29 +1,36 @@
 const Now = () => {
-    return ( 
-        <div className="text-center">   
-            <h1 className = "page_title">Now</h1>
-            <p>Updated February 3, from some where</p>
-            <p>This part is still underconstruction</p>
-            {/* <div className="felx flex-row ">
-                <div className="flex flex-row justify-between items-baseline">
-                    <h1 className="page_subtitle ">Prüfungspahse</h1>
-                    <p>03.02.2024</p>
-                </div>
-                <div>
-                    some Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, quo quas doloremque quis quam incidunt vero ex ab. Accusantium magnam laboriosam fuga aperiam, veritatis dolor numquam ab? Id, aut cum?
-                </div>
-            </div>
-            <div className="felx flex-row">
-                <div className="flex flex-col-2">
-                    <h1 className="page_subtitle">Prüfungspahse</h1>
-                    <p>03.02.2024</p>
-                </div>
-                <div>
-                    some Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, quo quas doloremque quis quam incidunt vero ex ab. Accusantium magnam laboriosam fuga aperiam, veritatis dolor numquam ab? Id, aut cum?
-                </div>
-            </div> */}
+    return (
+      <div className="text-center">   
+        <h1 className="page_title pb-2">Now</h1>
+        <p>Updated February 3rd 2024, from ETH library.</p>
+  
+        {/* Event Section */}
+        <div className="flex flex-col items-center pt-4">
+          <h1 className="text-3xl font-bold pt-2">Exam phase and webiste finalisation</h1>
+          <p className="py-2">03.02.2024</p>
+          <div className="max-w-[600px]  px-4">
+            <p>Only studying for one exam this seasion, OC III: Introduction to Asymmetric Synthesis. The lecture gives an  understanding to the principles of diastereoselective synthesis. Since this is my only exam I was able to 
+                finalize this homepage. I transitioned from static HTML on the servers of ETH to my own nextJS application using vercel. The final milestone of this homepage was the acquisition of the domain <i>dgoetz.net</i> </p>
+          </div>
         </div>
-     );
-}
- 
-export default Now;
+  
+        {/* Repeat for other events as needed */}
+        <div className="flex flex-col items-center pt-4">
+          <h1 className="text-3xl font-bold pt-2">Prüfungsphase</h1>
+          <p className="py-2">03.02.2024</p>
+          <div className="max-w-[600px]  px-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus et, quam aut facere deleniti optio delectus eius aspernatur distinctio repellat mollitia ducimus sed provident corrupti, neque architecto amet sapiente. Veritatis.</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center pt-4">
+          <h1 className="text-3xl font-bold pt-2">Prüfungsphase</h1>
+          <p className="py-2">03.02.2024</p>
+          <div className="max-w-[600px]  px-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus et, quam aut facere deleniti optio delectus eius aspernatur distinctio repellat mollitia ducimus sed provident corrupti, neque architecto amet sapiente. Veritatis.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Now;
