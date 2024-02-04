ed8c5e185641c45b19bf7133543f6307011d999f
Dominik Götz <goetzdominik@gmx.net>
2024-02-04 12:41:57 +0100

    pvk route created

---
+++ app/PVK/page.js
@@ -0,0 +1,12 @@
+const Teaching = () => {
+    return ( 
+        <div>
+            <h1 className = "page_title">
+                PVKs
+            </h1>
+            <p className=" text-center">At the moment this is empty but here you will find all the PVK content for each course. </p>
+        </div>
+     );
+}
+ 
+export default Teaching;
