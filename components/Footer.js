c8cdebade4a66bd8474f03840928b70c14ae0bb3
Dominik Götz <goetzdominik@gmx.net>
2024-02-04 12:48:25 +0100

    unnsecessary footer space removed

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (  
-        <div className="  flex items-center justify-center ">
-            <p className="items-center absolute bottom-0 p-4 pt-16">Copyright 2024 @ Dominik Götz</p>
+        <div className="  z-0 flex items-center justify-center ">
+            <p className="items-center absolute bottom-0 p-4 ">Copyright 2024 @ Dominik Götz</p>
         </div>
     );
 }
