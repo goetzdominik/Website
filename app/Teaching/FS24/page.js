a8d695464cafcd9b0056a7111e329d88ab89e949
Dominik Götz <goetzdominik@gmx.net>
2024-03-10 17:47:09 +0100

    new week

--- app/Teaching/FS24/page.js
+++ app/Teaching/FS24/page.js
@@ -118,6 +118,26 @@ const FS24 = () => {
                                     <Link href="/download/FS24/ACOC II LÖSUNG 2.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Solution 2</Link>
                                 </td> 
                             </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Acidity 
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                13.03.2024
+                                </td>
+                                <td className="px-6 py-4 ">
+                               
+                                </td>
+                                    
+                                <td className="px-6 py-4 ">
+                                    
+                                
+                                   
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/FS24/ACOCII_FS24_L3.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Solution 3</Link>
+                                </td> 
+                            </tr> 
                         </tbody>
                     </table>
                 </div>
