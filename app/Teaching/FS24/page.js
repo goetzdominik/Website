98a1534a8f7823ff3f7c2dce7be7d1e93b6971a6
Dominik <goetzdominik@gmx.net>
2024-04-22 13:45:26 +0200

    pre note week 7

--- app/Teaching/FS24/page.js
+++ app/Teaching/FS24/page.js
@@ -227,6 +227,28 @@ const FS24 = () => {
 
                                 </td> 
                             </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Functional Group Conversion
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                23.04.2024
+                                </td>
+                                <td className="px-6 py-4 ">
+                                <Link href="/download/FS24/ACOC_II_U8.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 8 </Link>
+                                
+                                </td>
+                                    
+                                <td className="px-6 py-4 ">
+                                    
+                                {/* <Link className = "text-blue-600 hover:underline" href="/download/FS24/ACOC_II_U7_menti.pdf" target="_blank">Menti 7</Link> */}
+
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/FS24/ACOCII_FS24_L7.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Solution 7</Link>
+
+                                </td> 
+                            </tr> 
                         </tbody>
                     </table>
                 </div>
