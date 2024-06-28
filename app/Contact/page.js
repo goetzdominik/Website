9740ce02cc3325e8adb9223df9b5d84e0e23b311
Dominik <goetzdominik@gmx.net>
2024-06-28 14:40:39 +0200

    removed mail from web

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -6,8 +6,8 @@ const Contact  = () => {
             <h1 className="page_title" > 
                 Contact 
             </h1>
-            <div className=" text_card_grid pt-8">
-                <div className =" md:w-2/6 text_card  ">
+            <div className=" text_card_grid pt-8 ml-24 mr-24">
+                <div className =" md:w-3/6 text_card  ">
                     <h1>
                     
                         <Link className = "text-blue-600 hover:underline" href="https://www.linkedin.com/in/dominikgötz/" target="_blank">Linked-In</Link>
@@ -17,18 +17,10 @@ const Contact  = () => {
                         If you are interested what I did in the past and want to keep up with what I am doing, follow me on linked-In!
                     </p>
                 </div>
-                <div className =" md:w-2/6 text_card">
-                <h1>
-                        <Link className = "text-blue-600 hover:underline"href={`mailto:${'dgoetz@ethz.ch'}`}target="_blank">Mail</Link>
-                        </h1>
-                    <p className=" ">
-                        
-                        If you want to get in touch with me, this is the fastest way! Also if you encounter a bug, please report this to me via mail.
-                    </p>
-                </div>
-                <div className = "md:w-2/6 text_card ">
+              
+                <div className = "md:w-3/6 text_card ">
                     <h1>
-                    <Link className = "text-blue-600 hover:underline" href="https://github.com/goetzdominik" target="_blank">Git-Hub</Link>
+                    <Link className = "text-blue-600 hover:underline" href="https://github.com/goetzdominik" target="_blank">GitHub</Link>
                     </h1>
                     <p>
                         On GitHub you can find all the codes that I referenced in presentations and soon also the code for this homepage.
