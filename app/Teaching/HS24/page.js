4ff6aa57f0ca4341cecb3c62fb5076acb075886e
Dominik <goetzdominik@gmx.net>
2024-11-24 19:36:22 +0100

    New week

--- app/Teaching/HS24/page.js
+++ app/Teaching/HS24/page.js
@@ -174,6 +174,30 @@ const HS24 = () => {
                                 </td>
                                
                             </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Reduction
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                18.11.2024
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/HS24/ex9.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 10</Link>
+                                </td>
+                               
+                            </tr> 
+                            {/* <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Oxidation
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                26.11.2024
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/HS24/ex10.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 11</Link>
+                                </td>
+                               
+                            </tr>  */}
                         </tbody>
                     </table>
                 </div>
