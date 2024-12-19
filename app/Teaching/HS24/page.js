f0d04ae93a0998f85f06e38a9c73eb0e504c8033
Dominik <goetzdominik@gmx.net>
2024-12-19 10:37:07 +0100

    last week

--- app/Teaching/HS24/page.js
+++ app/Teaching/HS24/page.js
@@ -222,6 +222,18 @@ const HS24 = () => {
                                 </td>
                                
                             </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Reactivity at alpha-Carbon III
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                17.12.2024
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/HS24/ex13.pdf" className=" text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 14</Link>
+                                </td>
+                               
+                            </tr> 
                         </tbody>
                     </table>
                 </div>
