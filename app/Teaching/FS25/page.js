6092ea4394b83b31c5ab010084fea00023e30b03
Dominik <goetzdominik@gmx.net>
2025-04-02 17:21:38 +0200

    new week

--- app/Teaching/FS25/page.js
+++ app/Teaching/FS25/page.js
@@ -161,6 +161,36 @@ const FS25 = () => {
                                 </td> 
                                 
                             </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                No Session
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                25.03.2025
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    {/* <Link href="/download/FS25/week4.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 4</Link> */}
+                                </td>
+                                <td className="px-6 py-4 ">    
+                                  
+                                </td> 
+                                
+                            </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Hammett-Plots
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                02.04.2025
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/FS25/week6.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 6</Link>
+                                </td>
+                                <td className="px-6 py-4 ">    
+                                  
+                                </td> 
+                                
+                            </tr> 
                         </tbody>
                     </table>
                 </div>
