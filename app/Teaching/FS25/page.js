c72855c27cd9501c1494e073d7a9daf846b04a5b
Dominik <goetzdominik@gmx.net>
2025-05-13 23:37:49 +0200

    new week

--- app/Teaching/FS25/page.js
+++ app/Teaching/FS25/page.js
@@ -288,6 +288,22 @@ const FS25 = () => {
                                 </td> 
                                 
                             </tr> 
+                            <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Electrophilic Addition II
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                13.05.2025
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/FS25/week13.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 13
+                                    </Link>
+                                </td>
+                                <td className="px-6 py-4 ">    
+                                  
+                                </td> 
+                                
+                            </tr> 
                         </tbody>
                     </table>
                 </div>
