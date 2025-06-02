d9868d9705cb7daa2b5b5c0a3bf485f797b78f4a
Dominik <goetzdominik@gmx.net>
2025-06-02 13:20:52 +0200

    new week plus sol

--- app/Teaching/FS25/page.js
+++ app/Teaching/FS25/page.js
@@ -320,6 +320,22 @@ const FS25 = () => {
                                 </td> 
                                 
                             </tr> 
+                             <tr className="odd:bg-white  even:bg-gray-0 border-b ">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900  ">
+                                Recap-Quizz
+                                </th>
+                                <td className="px-6 py-4 hidden md:block ">
+                                28.05.2025
+                                </td>
+                                <td className="px-6 py-4 ">
+                                    <Link href="/download/FS25/week15.pdf" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  target="_blank">Week 15
+                                    </Link>
+                                </td>
+                                <td className="px-6 py-4 ">    
+                                  
+                                </td> 
+                                
+                            </tr> 
                         </tbody>
                     </table>
                 </div>
